4273454f26435527439678e22d3a1555881c0486
VictorGillioz <v.a.gillioz@gmail.com>
2024-12-18 00:33:54 +0100

    add base code from langgraph template

---
+++ src/shared/retrieval.py
@@ -0,0 +1,67 @@
+"""Manage the configuration of various retrievers.
+
+This module provides functionality to create and manage retrievers for different
+vector store backends, specifically Elasticsearch, Pinecone, and MongoDB.
+"""
+
+import os
+from contextlib import contextmanager
+from typing import Generator
+
+from langchain_core.embeddings import Embeddings
+from langchain_core.runnables import RunnableConfig
+from langchain_core.vectorstores import VectorStoreRetriever
+
+from shared.configuration import BaseConfiguration
+
+## Encoder constructors
+
+
+def make_text_encoder(model: str) -> Embeddings:
+    """Connect to the configured text encoder."""
+    provider, model = model.split("/", maxsplit=1)
+    match provider:
+        case "openai":
+            from langchain_openai import OpenAIEmbeddings
+
+            return OpenAIEmbeddings(model=model)
+        case _:
+            raise ValueError(f"Unsupported embedding provider: {provider}")
+
+
+## Retriever constructors
+
+
+@contextmanager
+def make_chroma_retriever(
+    configuration: BaseConfiguration, embedding_model: Embeddings
+) -> Generator[VectorStoreRetriever, None, None]:
+    """Configure this agent to connect to a specific Chroma index."""
+    from langchain_chroma import Chroma
+
+    vstore = Chroma(
+        persist_directory=os.environ["VECTORSTORE_DIR"],
+        embedding_function=embedding_model,
+    )
+
+    yield vstore.as_retriever(search_kwargs=configuration.search_kwargs)
+
+
+@contextmanager
+def make_retriever(
+    config: RunnableConfig,
+) -> Generator[VectorStoreRetriever, None, None]:
+    """Create a retriever for the agent, based on the current configuration."""
+    configuration = BaseConfiguration.from_runnable_config(config)
+    embedding_model = make_text_encoder(configuration.embedding_model)
+    match configuration.retriever_provider:
+        case "chroma":
+            with make_chroma_retriever(configuration, embedding_model) as retriever:
+                yield retriever
+
+        case _:
+            raise ValueError(
+                "Unrecognized retriever_provider in configuration. "
+                f"Expected one of: {', '.join(BaseConfiguration.__annotations__['retriever_provider'].__args__)}\n"
+                f"Got: {configuration.retriever_provider}"
+            )
